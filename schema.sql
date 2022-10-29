cad96703a8f5de172b566c1200ae502184d25b51
Syed Shuja Hussain (He/Him) <shujashah786@gmail.com>
2022-10-30 00:38:25 +0500

    Revert "Feature2"

--- schema.sql
+++ schema.sql
@@ -1,13 +1,5 @@
 /* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL,
+    name varchar(100)
 );
-
-
-/* Add Column */
-ALTER TABLE animals ADD species VARCHAR(20);
