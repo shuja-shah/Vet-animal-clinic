1f01c883301ea54a802a638962e961fb010e3730
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-30 01:39:20 +0500

    Insert Data Specalization Table

--- data.sql
+++ data.sql
@@ -25,3 +25,11 @@ VALUES ('William Tatcher',45,'2000-04-23'),
 ('Maisy Smith',26,'2019-01-17'),
 ('Stephanie Mendez',64,'1981-05-04'),
 ('Jack Harkness',38,'2008-06-08');
+
+/* INSERT DATA INTO SPECALIZATION TABLE */
+INSERT INTO specializations (vets_id,species_id)
+ VALUES (1,1),
+ (3,1),
+ (3,2),
+ (4,2);
+
