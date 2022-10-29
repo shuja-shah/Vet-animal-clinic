fc487a20bc4986ae2f56ffdf3b9a9856facc1985
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-30 01:01:59 +0500

    INSTERT DATA

--- data.sql
+++ data.sql
@@ -2,3 +2,6 @@
 INSERT INTO animals (name,neutered,escape_attempts,date_of_birth,weight_kg) VALUES ('Agumon',TRUE,0,'2020-07-23',10.23),('Gabumon',TRUE,2,'2018-12-15',8), ('Pikachu',FALSE,1,'2021-01-7',15.08),('Devimon',TRUE,5,'2017-05-12',11);
 
 INSERT INTO animals (name,neutered,escape_attempts,date_of_birth,weight_kg) VALUES ('Charmander',FALSE,0,'2020-02-08',-11),('Plantmon',TRUE,2,'2021-12-15',-5.7), ('Squirtle',FALSE,3,'1993-04-02',-12.13),('Angemon',TRUE,1,'2005-06-12',-45),('Boarmon',TRUE,7,'2005-06-07',20.4),('Blossom',TRUE,3,'1998-10-13',17),('Ditto',TRUE,4,'2022-05-14',22);
+
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith',34),('Jennifer Orwell',19),('Bob',45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittake',38);
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
--- schema.sql
+++ schema.sql
@@ -21,3 +21,14 @@ age INT);
 CREATE TABLE IF NOT EXISTS species(
 id SERIAL PRIMARY KEY,
 name VARCHAR(50));
+
+
+/* Alter Table Animals */
+ ALTER TABLE animals DROP COLUMN species; 
+ ALTER TABLE animals ADD species_id INT;
+ ALTER TABLE animals ADD CONSTRAINT species_constraint FOREIGN KEY (species_id) references species (id);
+
+ ALTER TABLE animals ADD owner_id INT;
+ ALTER TABLE animals ADD CONSTRAINT owner_constraint FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+ 
